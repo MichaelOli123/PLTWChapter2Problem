019465cb3156a3cba1f638960c1fe32d42b6f62c
kevinc655 <130268284+kevinc655@users.noreply.github.com>
2024-12-12 13:46:41 -0800

    Update Scraper.java
    This will now function on the actual walmart website

--- Scraper.java
+++ Scraper.java
@@ -4,14 +4,15 @@ import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Scraper {
     private Document docu;
     public static void main(String[] args) throws IOException{
         Scraper scrape = new Scraper();
-        System.out.println("Print");
-        System.out.println(scrape.getReviews().text());
-        //scrape.getAllReviews();
+        //System.out.println(scrape.getNameReviewList());
+        scrape.getAllReviews();
         
     }
     public Scraper(String url) throws IOException{
@@ -22,22 +23,53 @@ public class Scraper {
         docu = Jsoup.parse(input, "UTF-8", "");
     }
 
-    public Elements getReviews(){
-        // return docu.select("span.tl-m db-m");
-        return docu.getElementsByClass("f7 b mv0"); // tl-m db-m is reviews
+    private Elements getReviews(){
+        // return docu.select("");
+        // return docu.getElementsByClass("");
+        return docu.getElementsByClass("tl-m db-m"); // tl-m db-m is reviews
+        /*
+         * f7 b mv0 is the name of the reviewers and "Sold by Walmart.com"
+         */
+    }
+
+    private Elements getNames(){
+        return docu.getElementsByClass("f7 b mv0");
+    }
+
+    public ArrayList<String> getNameReviewList(){
+        ArrayList<String> returnList = new ArrayList<String>();
+        ArrayList<String> names = new ArrayList<String>();
+        for (String name:getNames().text().split(" ")){
+            if (!"SoldbyWalmart.comPLWalmartCustomer".contains(name.strip())){
+                names.add(name.strip());
+            }
+        }
+        ArrayList<String> reviews = new ArrayList<String>();
+        for (String review:getReviews().html().split("<b></b>")){
+            if (review.length() > 0){
+                reviews.add(review);
+            }
+        }
+        for (int i = 0; i< names.size(); i++){
+            returnList.add(names.get(i) + "}" + reviews.get(i));
+        }
+        return returnList;
     }
 
     public void getAllReviews() throws IOException{
         int totalPages =  Integer.valueOf(getReviews().text().split(" reviews 5 stars")[0].substring(getReviews().text().split(" reviews 5 stars")[0].length()-3));
         FileWriter writer = new FileWriter("socialMediaPosts.txt");
         for (int i=1;i<((totalPages/10)+2);i++){
+        // for (int i=1; i<3; i++){
             System.out.println(i);
-            //docu = Jsoup.connect("https://www.walmart.com/reviews/product/5316226524?page=" + i).get();
-            // whatever function that gets the reviews here
-            // split the result of the function by whatever character it is, then write those to the text file
-            // with writer.write("text here")
-            // use writer.close() when done
+            docu = Jsoup.connect("https://www.walmart.com/reviews/product/5316226524?page=" + i).get(); // warning this will make 31 requests to the website in quick succession
+            ArrayList<String> toWrite = getNameReviewList();
+            for (String tw: toWrite){
+                String[] temp = tw.split("}");
+                writer.write(temp[0] + " "+temp[1]);
+            }
         }
+        writer.close();
 
     }
 }
